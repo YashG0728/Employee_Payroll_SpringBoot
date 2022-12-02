6933df49ea287db718a8bb211682053e8776a352
YashG0728 <yashgharde07@gmail.com>
2022-12-03 00:04:10 +0530

    Added : Funtionality to add validation in name

--- EmployeePayrollApp/src/main/java/com/example/employeepayrollapp/dto/EmployeeDto.java
+++ EmployeePayrollApp/src/main/java/com/example/employeepayrollapp/dto/EmployeeDto.java
@@ -1,10 +1,14 @@
 package com.example.employeepayrollapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class EmployeeDto {
     private int employeeId;
+    @NotEmpty(message = "Employee cannot be null")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     private String name;
     private long salary;
 
